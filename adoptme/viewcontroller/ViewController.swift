d0aaecb1aefa469994e2e3bf0eb063c2ea523760
Raúl Rodríguez Concepción <>
2019-08-30 21:43:18 +0200

    material button

--- adoptme/viewcontroller/ViewController.swift
+++ adoptme/viewcontroller/ViewController.swift
@@ -7,14 +7,20 @@
 //
 
 import UIKit
+import MaterialComponents
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var btLogin: MDCButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+  
+    
 }
 
